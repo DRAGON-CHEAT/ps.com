064657088fb2fe2bb7ca59ca6f0618145bffb1d4
dragon-cheatPkg-ÇáÕíÇ/goldheÇá999ps4لãäÇÝÓÉ ÇáÕíÇäÉ ÇáØÈíÉ ÇáãæÍÏÉeiÇáØÈíÉhchac/khlfÇáÕíÇäÉ ÇáØÈ/íÉgjubgFggitgTitiYÇáÕíÇufufufFäÉ hjfhdhhvg66tvjv73ÇáÕí/ÇäÉ ÇáØÈíÉ54iueSdf66fGufguttCi/bvfggFiitbihgtib gighfhdhcjgitubio/givcbnjitbnjfrv <120471333+DRAGON-CHEAT@users.noreply.github.com>
2025-12-19 02:09:37 +0300

    Add 'README.md' to excluded files list

--- 900v2.3/CacheMaker.py
+++ 900v2.3/CacheMaker.py
@@ -7,7 +7,7 @@ EXCLUDED_EXTENSIONS = {
     '.bat', '.txt', '.exe', '.mp4', '.py', '.bak', '.zip',
     '.mp3', '.sh', '.h', '.c', '.o', '.ld', '.md', '.d', '.LICENSE'
 }
-EXCLUDED_FILES = {'.gitignore', '.keepgithub', 'COPYING', 'LICENSE', 'MAKEFILE', 'Makefile', '.dockerignore',  'dockerfile', '.gitinclude'}
+EXCLUDED_FILES = {'.gitignore', '.keepgithub', 'COPYING', 'LICENSE', 'MAKEFILE', 'Makefile', 'README.md', '.dockerignore', 'dockerfile', '.gitinclude'}
 OUTPUT_FILE = 'PSFree.manifest'
 
 def create_manifest():
